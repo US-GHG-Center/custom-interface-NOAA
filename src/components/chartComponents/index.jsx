45bbe0173eda2999268acf442d3ed2fd574ad626
Sandesh Pandey <sandeshsinbox@gmail.com>
2025-04-21 22:54:58 -0500

    Change CSS of chart component

--- src/components/chartComponents/index.jsx
+++ src/components/chartComponents/index.jsx
@@ -120,7 +120,7 @@ export const DataAccessTool = ({ dataAccessLink, tooltip }) => {
         data-tooltip-content={tooltip}
         class='data-access-tool'
       >
-        <FontAwesomeIcon icon={faExternalLink} />
+        {tooltip} <FontAwesomeIcon icon={faExternalLink} />
       </a>
       <Tooltip id="data-access-tooltip" place="bottom" style={tooltipStyle} />
     </>
