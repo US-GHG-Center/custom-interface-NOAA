86f62feddbf85e6da7f1640338a43654b44bbd7c
Sandesh Pandey <sandeshsinbox@gmail.com>
2026-05-19 19:12:20 -0500

    fix: prevent Mapbox markers from re-rendering on click

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -261,35 +261,28 @@ export function Dashboard({
               <img src={logo} alt='NOAA' className='logo' />
 
               <MapZoom zoomLocation={zoomLocation} zoomLevel={zoomLevel} />
-              {vizItems.map((item) => {
+              {vizItems.flatMap((item) => {
                 const [category, data] = Object.entries(item)[0];
                 if (
                   selectedFrequency === 'all' ||
                   selectedFrequency === category
                 ) {
-                  const stations = Object.values(data.stations).map(
-                    (station) => {
-                      return {
-                        id: station?.id,
-                        coordinates: {
-                          lon: station?.geometry?.coordinates[0][0][0],
-                          lat: station?.geometry?.coordinates[0][0][1],
-                        },
-                        station: station,
-                      };
-                    }
-                  );
-                  const stationClass = `${data?.color}-${data?.stations.length}`;
-                  return (
+                  return Object.values(data.stations).map((station) => (
                     <MarkerFeature
-                      key={stationClass}
-                      items={stations}
+                      key={`${category}-${station?.id}`}
+                      id={station?.id}
+                      coordinates={{
+                        lon: station?.geometry?.coordinates[0][0][0],
+                        lat: station?.geometry?.coordinates[0][0][1],
+                      }}
+                      station={station}
                       markerColor={data.color}
                       onSelectVizItem={handleSelectedVizItem}
                       getPopupContent={getPopUpContent}
                     />
-                  );
+                  ));
                 }
+                return [];
               })}
               <FrequencyDropdown
                 selectedValue={selectedFrequency}
